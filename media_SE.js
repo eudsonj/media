4953bef3198e26f578ff70a207c06f393b97e711
eudsonj <silva.eudson@escola.pr.gov.br>
2026-02-24 09:49:34 -0300

    Add files via upload

---
+++ media_SE.js
@@ -0,0 +1,16 @@
+let alunos="Robson";
+let+ N1= 99;
+let+ N2= 100;
+let+ N3= 99;
+let+ N4= 100;
+let+ media= 0
+let result= " ";
+
+media=(N1+N2+N3+N4)/4;
+ 
+if(media>=70){
+    result="aprovado"
+}else{
+    result="reprovado"
+}
+console.log("O aluno"+aluno+"tem a media"+media+"e está:"+result);
